60ab0040ce42d58ce697c39ae7d5fa1ac6b10580
GGamerE <192621951+GGamerE@users.noreply.github.com>
2025-08-25 10:11:10 +0800

    use encrypt address to save passport code

--- contracts/SecureKYC.sol
+++ contracts/SecureKYC.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {FHE, euint32, euint8, euint256, externalEuint32, externalEuint8, externalEuint256, ebool} from "@fhevm/solidity/lib/FHE.sol";
+import {
+    FHE,
+    euint32,
+    euint8,
+    euint256,
+    eaddress,
+    externalEuint32,
+    externalEuint8,
+    externalEaddress,
+    ebool
+} from "@fhevm/solidity/lib/FHE.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
 /// @title SecureKYC - A privacy-preserving KYC verification system
@@ -9,7 +19,7 @@ import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 /// @dev All KYC data is stored encrypted and can only be accessed by authorized parties
 contract SecureKYC is SepoliaConfig {
     struct EncryptedKYCData {
-        euint256 passportHash;
+        eaddress passportAddress;
         euint32 birthYear;
         euint8 countryCode;
         bool isVerified;
@@ -28,9 +38,9 @@ contract SecureKYC is SepoliaConfig {
     mapping(address => bool) public authorizedVerifiers;
     mapping(bytes32 => KYCRequirement) public projectRequirements;
     mapping(address => mapping(bytes32 => bool)) public userProjectEligibility;
-    
+
     address public admin;
-    
+
     event KYCVerified(address indexed user, address indexed verifier, uint256 timestamp);
     event VerifierAuthorized(address indexed verifier, bool authorized);
     event ProjectRequirementSet(bytes32 indexed projectId, uint32 minAge, bool requiresPassport);
@@ -61,22 +71,22 @@ contract SecureKYC is SepoliaConfig {
     }
 
     /// @notice Submit KYC data for verification
-    /// @param passportHash Encrypted hash of passport number
+    /// @param passportAddress Encrypted passport number as address
     /// @param birthYear Encrypted birth year
     /// @param countryCode Encrypted country code (1-255)
     /// @param inputProof Proof for the encrypted inputs
     function submitKYC(
-        externalEuint256 passportHash,
+        externalEaddress passportAddress,
         externalEuint32 birthYear,
         externalEuint8 countryCode,
         bytes calldata inputProof
     ) external {
-        euint256 encryptedPassport = FHE.fromExternal(passportHash, inputProof);
+        eaddress encryptedPassportAddress = FHE.fromExternal(passportAddress, inputProof);
         euint32 encryptedBirthYear = FHE.fromExternal(birthYear, inputProof);
         euint8 encryptedCountryCode = FHE.fromExternal(countryCode, inputProof);
 
         userKYCData[msg.sender] = EncryptedKYCData({
-            passportHash: encryptedPassport,
+            passportAddress: encryptedPassportAddress,
             birthYear: encryptedBirthYear,
             countryCode: encryptedCountryCode,
             isVerified: false,
@@ -84,11 +94,11 @@ contract SecureKYC is SepoliaConfig {
             verifiedBy: address(0)
         });
 
-        FHE.allowThis(encryptedPassport);
+        FHE.allowThis(encryptedPassportAddress);
         FHE.allowThis(encryptedBirthYear);
         FHE.allowThis(encryptedCountryCode);
-        
-        FHE.allow(encryptedPassport, msg.sender);
+
+        FHE.allow(encryptedPassportAddress, msg.sender);
         FHE.allow(encryptedBirthYear, msg.sender);
         FHE.allow(encryptedCountryCode, msg.sender);
     }
@@ -96,7 +106,7 @@ contract SecureKYC is SepoliaConfig {
     /// @notice Verify a user's KYC data (only authorized verifiers)
     /// @param user Address of the user to verify
     function verifyKYC(address user) external onlyAuthorizedVerifier {
-        if (!FHE.isInitialized(userKYCData[user].passportHash)) {
+        if (!FHE.isInitialized(userKYCData[user].passportAddress)) {
             revert InvalidInput();
         }
 
@@ -145,27 +155,27 @@ contract SecureKYC is SepoliaConfig {
         euint32 currentYear = FHE.asEuint32(uint32(block.timestamp / 365 days + 1970));
         euint32 userAge = FHE.sub(currentYear, userKYCData[user].birthYear);
         euint32 minAge = FHE.asEuint32(requirements.minAge);
-        
+
         ebool ageEligible = FHE.ge(userAge, minAge);
-        
+
         ebool countryEligible = FHE.asEbool(false);
         for (uint i = 0; i < requirements.allowedCountries.length; i++) {
             euint8 allowedCountry = FHE.asEuint8(requirements.allowedCountries[i]);
             ebool countryMatch = FHE.eq(userKYCData[user].countryCode, allowedCountry);
             countryEligible = FHE.or(countryEligible, countryMatch);
         }
-        
+
         ebool passportEligible = FHE.asEbool(true);
         if (requirements.requiresPassport) {
-            passportEligible = FHE.ne(userKYCData[user].passportHash, FHE.asEuint256(0));
+            passportEligible = FHE.ne(userKYCData[user].passportAddress, FHE.asEaddress(address(0)));
         }
-        
+
         eligible = FHE.and(FHE.and(ageEligible, countryEligible), passportEligible);
-        
+
         FHE.allowThis(eligible);
         FHE.allow(eligible, msg.sender);
         FHE.allow(eligible, user);
-        
+
         return eligible;
     }
 
@@ -174,20 +184,20 @@ contract SecureKYC is SepoliaConfig {
     /// @return proof Encrypted proof of eligibility
     function generateProof(bytes32 projectId) external returns (euint256 proof) {
         ebool eligible = this.checkEligibility(msg.sender, projectId);
-        
+
         euint256 proofValue = FHE.select(
             eligible,
             FHE.asEuint256(uint256(keccak256(abi.encodePacked(msg.sender, projectId, block.timestamp)))),
             FHE.asEuint256(0)
         );
-        
+
         userProjectEligibility[msg.sender][projectId] = true;
-        
+
         FHE.allowThis(proofValue);
         FHE.allow(proofValue, msg.sender);
-        
+
         emit EligibilityChecked(msg.sender, projectId, true);
-        
+
         return proofValue;
     }
 
@@ -201,12 +211,12 @@ contract SecureKYC is SepoliaConfig {
 
     /// @notice Get user's encrypted KYC data
     /// @param user Address of the user
-    /// @return passportHash Encrypted passport hash
+    /// @return passportAddress Encrypted passport address
     /// @return birthYear Encrypted birth year
     /// @return countryCode Encrypted country code
-    function getUserKYCData(address user) external view returns (euint256, euint32, euint8) {
+    function getUserKYCData(address user) external view returns (eaddress, euint32, euint8) {
         EncryptedKYCData memory data = userKYCData[user];
-        return (data.passportHash, data.birthYear, data.countryCode);
+        return (data.passportAddress, data.birthYear, data.countryCode);
     }
 
     /// @notice Check if user is verified
@@ -214,7 +224,9 @@ contract SecureKYC is SepoliaConfig {
     /// @return verified Whether user is verified
     /// @return timestamp Verification timestamp
     /// @return verifier Address of verifier
-    function getVerificationStatus(address user) external view returns (bool verified, uint256 timestamp, address verifier) {
+    function getVerificationStatus(
+        address user
+    ) external view returns (bool verified, uint256 timestamp, address verifier) {
         EncryptedKYCData memory data = userKYCData[user];
         return (data.isVerified, data.verificationTimestamp, data.verifiedBy);
     }
